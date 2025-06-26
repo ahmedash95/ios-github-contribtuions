429c15f2c4abc2fa60d8ccd0e4d861acfcbb56f4
Ahmed Ammar <ahmed29329@gmail.com>
2025-06-27 01:31:39 +0200

    Initial Commit

---
+++ contributions/contributionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  contributionsApp.swift
+//  contributions
+//
+//  Created by Ahmed on 27.06.25.
+//
+
+import SwiftUI
+
+@main
+struct contributionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
